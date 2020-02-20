698eecec90a44a3354d014ec8d5477e94f4b109f
Esterlin Polanco <Esterlinpol@gmail.com>
2020-02-20 10:45:10 -0400

    Update fisica_2.ino

--- fisica_2.ino
+++ fisica_2.ino
@@ -1,5 +1,47 @@
+/*------------------------------------------------------------------------------
+  20/02/2020
+  Author: Esterlin Polanco
+  Platforms: ESP8266
+  Language: C++/Arduino
+  File: fisica_2.ino
+  ------------------------------------------------------------------------------
+  Description: 
+  Code for Water control system, built for physics 2 class at Dominico Americano
+  University, Santo Domingo D.R.
+  ------------------------------------------------------------------------------
+  License:
+  Please see attached LICENSE.txt file for details.
+------------------------------------------------------------------------------*/
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+ESP8266WebServer server;
+
+char* ssid = "Flow";
+char* password = "12345678";
+
+IPAddress local_ip(192,168,10,4);
+IPAddress gateway(192,168,10,1);
+IPAddress subnet(255,255,255,0);
+
 void setup() {
-  // put your setup code here, to run once:
+WiFi.softAP(ssid, password)
+softAPConfig (local_ip, gateway, subnet)
+
+Serial.begin(115200);
+
+Serial.println();
+
+Serial.print("Setting soft-AP configuration ... ");
+
+Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+
+Serial.print("Setting soft-AP ... ");
+
+Serial.println(WiFi.softAP("ESPsoftAP_01") ? "Ready" : "Failed!");
+
+Serial.print("Soft-AP IP address = ");
+
+Serial.println(WiFi.softAPIP());
 
 }
 
