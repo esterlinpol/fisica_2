1f8d7fb6da07d41958a190e8ef9f993e05a9bfbd
Esterlin Polanco <esterlinpol@gmail.com>
2020-02-21 00:16:38 -0400

    home made
    -Includes sonar 1 working now.
    -Distance and percentage is measurable now.

--- fisica_2.ino
+++ fisica_2.ino
@@ -15,8 +15,11 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
+#include <HCSR04.h>
 ESP8266WebServer server;
-//Set Website content:
+/*------------------------------------------------------------------------------
+  Set Website content:
+------------------------------------------------------------------------------*/
 const char responseHTML[] PROGMEM={"<!DOCTYPE html>\n"
 "<html>\n"
 "<head>\n"
@@ -31,9 +34,13 @@ const char responseHTML[] PROGMEM={"<!DOCTYPE html>\n"
 "</html>\n"
 };
 
+/*------------------------------------------------------------------------------
+  Network and Web server configuration:
+------------------------------------------------------------------------------*/
+
 //Define WiFi Name and password:
-char* ssid = "Flow";
-char* password = "12345678";
+const char* ssid = "Flow";
+const char* password = "SafePass123";
 
 //Set IP address GW and subnet:
 IPAddress local_ip(192,168,10,4);
@@ -47,41 +54,90 @@ DNSServer dnsServer;
 //Define HTTP Server:
 ESP8266WebServer webServer(80);
 
+/*------------------------------------------------------------------------------
+  Sonar 1 configuration:
+------------------------------------------------------------------------------*/
+
+//Define Pins for sonas module 1:
+#define trigPin1 5
+#define echoPin1 4
+int heightTank1=10; //Set Height of the container
+int deviation1=2; //Set Distance from the maximun height of the liquid
+int duration1,distance1,percentage1; 
+
+/*------------------------------------------------------------------------------
+  Startup code:
+------------------------------------------------------------------------------*/
 
 void setup() {
 
+  pinMode(trigPin1,OUTPUT); //Sets the trigPin1 as an Output
+  pinMode(echoPin1,INPUT); //Sets the echoPin1 as an Input
 
-//Open Serial Port:
-Serial.begin(115200);
+  //Open Serial Port:
+  Serial.begin(115200);
 
-Serial.println();
-//Set Settings:
-Serial.print("Setting soft-AP configuration ... ");
+/*------------------------------------------------------------------------------
+  WIFI AP Configuration:
+------------------------------------------------------------------------------*/
 
-Serial.println(WiFi.softAPConfig(local_ip, gateway, subnet) ? "Ready" : "Failed!");
+  Serial.println();
+  //Set Settings:
+  Serial.print("Setting soft-AP configuration ... ");
 
-//Start WiFi in AP mode:
-Serial.print("Setting soft-AP ... ");
+  Serial.println(WiFi.softAPConfig(local_ip, gateway, subnet) ? "Ready" : "Failed!");
 
-Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
+  //Start WiFi in AP mode:
+  Serial.print("Setting soft-AP ... ");
 
-Serial.print("Soft-AP IP address = ");
+  Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
 
-Serial.println(WiFi.softAPIP());
+  Serial.print("Soft-AP IP address = ");
 
-//Captive portal control:
-dnsServer.start(DNS_PORT, "*", local_ip);
+  Serial.println(WiFi.softAPIP());
 
-//Reply all request with same page:
-webServer.onNotFound([]() {
-  webServer.send(200, "text/html", responseHTML);
-  });
-  webServer.begin();
+/*------------------------------------------------------------------------------
+  Captive portal and Web server start:
+------------------------------------------------------------------------------*/
+
+  //Captive portal control:
+  dnsServer.start(DNS_PORT, "*", local_ip);
+
+  //Reply all request with same page:
+  webServer.onNotFound([]() {
+    webServer.send(200, "text/html", responseHTML);
+    });
+    webServer.begin();
 
 
 }
+
+/*------------------------------------------------------------------------------
+  Loop code:
+------------------------------------------------------------------------------*/
 
 void loop() {
-dnsServer.processNextRequest();
- webServer.handleClient();
+
+  dnsServer.processNextRequest(); //DNS Server handler
+  webServer.handleClient(); //WEB Server handler
+  sonar1(); //Sonar 1 value reporter
 }
+
+/*------------------------------------------------------------------------------
+  Sonar 1 code, data colector:
+------------------------------------------------------------------------------*/
+void sonar1(){
+  //Pulse and Receive:
+  digitalWrite(trigPin1,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin1,LOW);
+  duration1=pulseIn(echoPin1,HIGH);
+  //Calculate distance and percentage:
+  distance1=(duration1/2)/29.1;
+  percentage1=100-(((distance1-deviation1)*100)/heightTank1);
+  //Print results:
+  Serial.println(distance1);
+  Serial.println(percentage1);
+  delay(1000);
+
+}  
