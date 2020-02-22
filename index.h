cc99a223a3f00c77d7edaed6a3b6274d73c8644f
Esterlin Polanco <esterlinpol@gmail.com>
2020-02-22 19:19:50 -0400

    EOD
    -Added Second cylinder.
    -had a good time

--- index.h
+++ index.h
@@ -1,127 +1,231 @@
 const char MAIN_page[] PROGMEM = R"=====(
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Distribucion de Fluidos</title>
-        <style>
-            html { 
-                background-color: black;
-                color: white;
-                font-family: Helvetica; 
-                display: inline-block; 
-                margin: 0px auto; 
-                text-align: center;
-                }
-            :root {
-                
-                --height: 50px;    
-                }  
-
-            .cylinder {
-                position: relative;
-                overflow: hidden;
-                margin: 0 auto;
-                width: 100px;
-                height: 260px;
-                border-radius: 50px/25px;
-                background-color: rgba(160, 160, 160, 0.89);
-                }
-            .cylinder:before {
-                position: absolute;
-                left: 0;
-                top: 0;
-                width: 100px;
-                height: 50px;
-                border-radius: 50px/25px;
-                background-color: rgba(37, 33, 33, 0.2);
-                content: '';
-                }
-            .cylinder:after {
-                position: absolute;
-                left: 0;
-                bottom: 0;
-                width: 100px;
-                height: 50px;
-                border-radius: 50px/25px;
-                background-color: rgba(37, 33, 33, 0.2);
-                content: '';
-                }
-            .water {
-                position: absolute;
-                left: 0;
-                bottom: 0;
-                width: 100px;
-                           
-                padding-top: var(--height);
-                border-radius: 50px/25px;
-                background-color: rgba(0, 136, 160, 0.568);
-                transition: 0.3s linear;
-                }    
-            .water:before {
-                position: absolute;
-                left: 0;
-                top: 0;
-                width: 100px;
-                height: 50px;
-                border-radius: 50px/25px;
-                background-color: rgba(4, 118, 138, 0.699);
-                content: '';
-                }
-            .water:after {
-                position: absolute;
-                left: 0;
-                bottom: 0;
-                width: 100px;
-                height: 50px;
-                border-radius: 50px/25px;
-                background-color: rgba(63, 243, 243, 0.4);
-                content: '';
-                }
-            p {
-                display:inline;
-            }
-            span {
-                display:inline;
-            }        
-                  
-                
-        </style>
-    </head>
-    <body>
-        <div id="webpage">
-            <h1>Nivel de agua en contenedor:</h1>
-            <span id="sonar1value">0</span><p>%</p>
-        </div>
-        <div class="cylinder">
-            <div class="water"></div>
-        </div>
-        <script>
-            setInterval(function() {
-            // Call a function repetatively with 2 Second interval
-            getData();
-            }, 2000); //2000mSeconds update rate
-            
-            function getData() {
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                      document.getElementById("sonar1value").innerHTML =
-                      this.responseText;
-                      var style = getComputedStyle(document.body);
-                var calculado = this.responseText * 2.5;
-                          
-                document.documentElement.style.setProperty('--height', calculado+'px');
-                
-                      
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Distribucion de Fluidos</title>
+            <style>
+                html { 
+                    background-color: black;
+                    color: white;
+                    font-family: Helvetica; 
+                    display: inline-block; 
+                    margin: 0px auto; 
+                    text-align: center;
                     }
-                  };
-                  xhttp.open("GET", "sonar1", true);
-                  
-                  xhttp.send();
-
+                :root {
+                    
+                    --height: 50px;
+                    --height1: 50px;    
+                    }  
+    
+                .container {
+                    width: 100%;
+                    margin: auto;
+                    padding: 10px;
+                    }
+                .cylinder {
+                    float: left;
+                    margin-left:10%;
+                    position: relative;
+                    overflow: hidden;
+                    
+                    width: 100px;
+                    height: 260px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(160, 160, 160, 0.89);
+                    }
+                .cylinder:before {
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    width: 100px;
+                    height: 50px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(37, 33, 33, 0.2);
+                    content: '';
+                    }
+                .cylinder:after {
+                    
+                    position: absolute;
+                    left: 0;
+                    bottom: 0;
+                    width: 100px;
+                    height: 50px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(37, 33, 33, 0.2);
+                    content: '';
+                    }
+                .water {
+                    
+                    position: absolute;
+                    left: 0;
+                    bottom: 0;
+                    width: 100px;
+                               
+                    padding-top: var(--height);
+                    border-radius: 50px/25px;
+                    background-color: rgba(0, 136, 160, 0.568);
+                    transition: 0.3s linear;
+                    }    
+                .water:before {
+                    
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    width: 100px;
+                    height: 50px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(4, 118, 138, 0.699);
+                    content: '';
+                    }
+                .water:after {
+                    
+                    position: absolute;
+                    left: 0;
+                    bottom: 0;
+                    width: 100px;
+                    height: 50px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(63, 243, 243, 0.4);
+                    content: '';
+                    }
+                    .cylinder1 {
+                    float: right;
+                    margin-right:10%;
+                    position: relative;
+                    overflow: hidden;
+                    
+                    width: 100px;
+                    height: 260px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(160, 160, 160, 0.89);
+                    }
+                .cylinder1:before {
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    width: 100px;
+                    height: 50px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(37, 33, 33, 0.2);
+                    content: '';
+                    }
+                .cylinder1:after {
+                    
+                    position: absolute;
+                    left: 0;
+                    bottom: 0;
+                    width: 100px;
+                    height: 50px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(37, 33, 33, 0.2);
+                    content: '';
+                    }
+                .water1 {
+                    
+                    position: absolute;
+                    left: 0;
+                    bottom: 0;
+                    width: 100px;
+                               
+                    padding-top: var(--height1);
+                    border-radius: 50px/25px;
+                    background-color: rgba(0, 136, 160, 0.568);
+                    transition: 0.3s linear;
+                    }    
+                .water1:before {
+                    
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    width: 100px;
+                    height: 50px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(4, 118, 138, 0.699);
+                    content: '';
+                    }
+                .water1:after {
+                    
+                    position: absolute;
+                    left: 0;
+                    bottom: 0;
+                    width: 100px;
+                    height: 50px;
+                    border-radius: 50px/25px;
+                    background-color: rgba(63, 243, 243, 0.4);
+                    content: '';
+                    }                
+                                
+                .vdescl {
+                    float: left;
+                    position: relative;
+                    margin-left:15%;
+    
+                            
                 }
-        </script>    
-    </body>
-</html>
+                .vdescr {
+                    float: right;
+                    position: relative;
+                    margin-right:16%;
+           
+                }
+                p{
+                    font-weight: bold;
+                }      
+                    
+            </style>
+        </head>
+        <body>
+            <div id="webpage">
+                <h1>Nivel de agua en Contenedores:</h1>
+                
+            </div>
+            <section class="container">
+                <div class="vdescl">
+                    <p>Cisterna(%):</p>
+                    <span id="sonar1value">100%</span>
+                </div>
+                <div class="vdescr">    
+                    <p>Tinaco(%):</p>
+                    <span id="sonar1value">100%</span>                
+                </div>    
+            </section>            
+            <section class="container">
+                <div class="cylinder">
+                    <div class="water"></div>
+                </div>
+                <div class="cylinder1">
+                        <div class="water1"></div>
+                    </div>
+            </section>
+            <script>
+                    setInterval(function() {
+                    // Call a function repetatively with 2 Second interval
+                    getData();
+                    }, 2000); //2000mSeconds update rate
+                    
+                    function getData() {
+                        var xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = function() {
+                            if (this.readyState == 4 && this.status == 200) {
+                              document.getElementById("sonar1value").innerHTML =
+                              this.responseText;
+                              var style = getComputedStyle(document.body);
+                        var calculado = this.responseText * 2.5;
+                                  
+                        document.documentElement.style.setProperty('--height', calculado+'px');
+                        document.documentElement.style.setProperty('--height1', this.responseText+'px');
+                        
+                              
+                            }
+                          };
+                          xhttp.open("GET", "sonar1", true);
+                          
+                          xhttp.send();
+        
+                        }
+            </script>           
+        </body>
+    </html>
 )=====";
